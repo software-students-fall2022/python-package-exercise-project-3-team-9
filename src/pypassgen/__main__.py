5a3a1ac04acd59b53679e6f16c098c15e70ae885
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2022-10-28 00:43:32 -0400

    Error catching in main

--- src/pypassgen/__main__.py
+++ src/pypassgen/__main__.py
@@ -15,9 +15,16 @@ def main():
     choice = ""
     while choice != "5":
         menu()
-        choice = input("Enter your choice: ")
+        choice = input("Enter your choice (1 - 5): ")
         if (choice == "1"):
             print("Your password is: " + wisdom.generate_password() + "\n")
+        # elif (choice == "2"):
+        # elif (choice == "3"):
+        # elif (choice == "4"):
+        elif (choice == "5"):
+            print("Goodbye!\n")
+        else:
+            print("Invalid choice. Please try again or enter 5 to exit.\n")
 
 
 if __name__ == '__main__':
