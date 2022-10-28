3324812206c2d830b2cff9c67a49c88c8fbe5667
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2022-10-27 23:55:35 -0400

    generate password functional

--- src/pypassgen/wisdom.py
+++ src/pypassgen/wisdom.py
@@ -2,13 +2,12 @@ import random
 import string
 
 
-def generate_password(length=8, num=0, alpha=0, upper=0, lower=0, special=0):
+def generate_password(length=8, num=0, upper=0, lower=0, special=0):
     """Generate a random password based on user input
 
     Keyword arguments:
         length -- the minimal length of the password to generate (default 8)
         num -- the minimal number of numeric characters required in the password (default 0)
-        alpha -- the minimal number of alphabetic characters required in the password (default 0)
         upper -- the minimal number of uppercase characters required in the password (default 0)
         lower -- the minimal number of lowercase characters required in the password (default 0)
         special -- the minimal number of special characters required in the password (default 0)
@@ -16,15 +15,30 @@ def generate_password(length=8, num=0, alpha=0, upper=0, lower=0, special=0):
         A string containing the generated password
     """
     # Catch errors
-    if length < 8 or num < 0 or alpha < 0 or upper < 0 or lower < 0 or special < 0:
+    if length < 1 or num < 0 or upper < 0 or lower < 0 or special < 0:
         print("Invalid input. Please try again.")
-    if (upper + lower) > alpha:
-        print("Error: The number of uppercase and lowercase characters cannot exceed the number of alphabetic characters")
 
     # all possible characters
     base = string.digits + string.ascii_letters + string.punctuation
     password = ""
-    print(base)
+    if (num != 0):
+        password += ''.join(random.choice(string.digits) for _ in range(num))
+    if (upper != 0):
+        password += ''.join(random.choice(string.ascii_uppercase)
+                            for _ in range(upper))
+    if (lower != 0):
+        password += ''.join(random.choice(string.ascii_lowercase)
+                            for _ in range(lower))
+    if (special != 0):
+        password += ''.join(random.choice(string.punctuation)
+                            for _ in range(special))
+    # generate the rest of the password if needed
+    if (length - len(password) > 0):
+        password += ''.join(random.choice(base)
+                            for _ in range(length - len(password)))
+    # shuffle the password
+    password = ''.join(random.sample(password, len(password)))
+    print(password)
 
 
 generate_password()
