e08d0e130fa6d3b7b9afc137cde34dfae6a7c355
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2022-10-28 00:31:46 -0400

    BASIC main.py

--- src/pypassgen/__main__.py
+++ src/pypassgen/__main__.py
@@ -1,8 +1,23 @@
-import pypassgen.wisdom as wisdom
+import wisdom as wisdom
+
+
+def menu():
+    print("Welcome to pypassgen - your simple and lightweight password generator!")
+    # Prompt user to enter a pick which function to use
+    print("1. Generate a password")
+    print("2. Verify a password")
+    print("3. Encrypt a phrase")
+    print("4. Decrypt an encrypted phrase")
+    print("5. Exit")
 
 
 def main():
-    print("Welcome to pypassgen - your simple and lightweight password generator!")
+    choice = ""
+    while choice != "5":
+        menu()
+        choice = input("Enter your choice: ")
+        if (choice == "1"):
+            print("Your password is: " + wisdom.generate_password() + "\n")
 
 
 if __name__ == '__main__':
