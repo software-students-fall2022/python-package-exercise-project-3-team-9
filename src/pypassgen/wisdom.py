b44672d84cdc248aa70363f04acc6a75bc7b0441
elyazya <113930251+elyazya@users.noreply.github.com>
2022-10-29 09:38:06 -0400

    adding decrypt function

--- src/pypassgen/wisdom.py
+++ src/pypassgen/wisdom.py
@@ -251,7 +251,7 @@ def decryption(str):
         'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
         'u', 'v', 'w', 'x', 'y', 'z'
     ]
-    #decoding decrypted text by shifting
+    #decoding decryppted text by shifting
     decoded_text= ""
     for char in str_decrypted:
         if (char.isdigit() or char.isalpha()):
