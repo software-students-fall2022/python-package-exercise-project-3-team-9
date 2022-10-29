b76219a9a442867b874acd9baaa1e336c6b370d9
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2022-10-28 20:04:25 -0400

    all tests for encryption

--- tests/test_with_pytest.py
+++ tests/test_with_pytest.py
@@ -106,7 +106,7 @@ def test_special_input():
     assert (cipher_suite.decrypt(encoded)).decode("utf-8") == input
 
 
-def test_random_input():
+def test_alphanumeric_input():
     # Test random input
     arr = [
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
@@ -125,6 +125,34 @@ def test_random_input():
     for char in input:
         idx = arr.index(char)
         str_encrypted += arr[(idx+5) % len(arr)]
+    key = open(wisdom.getFile("key.txt"), "rb").read()
+    cipher_suite = Fernet(key)
+    encoded = wisdom.encryption(input)
+    assert (cipher_suite.decrypt(encoded)).decode('utf-8') == str_encrypted
+
+
+def test_all_input():
+  # Test random input
+    arr = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z'
+    ]
+    input = "".join(
+        random.sample(
+            string.ascii_lowercase + string.digits + string.punctuation,
+            len(string.ascii_lowercase + string.digits + string.punctuation),
+        )
+    )
+    print("Input: " + input)
+    str_encrypted = ""
+    for char in input:
+        if (char.isdigit() or char.isalpha()):
+            idx = arr.index(char)
+            str_encrypted += arr[(idx+5) % len(arr)]
+        else:
+            str_encrypted += char
     key = open(wisdom.getFile("key.txt"), "rb").read()
     cipher_suite = Fernet(key)
     encoded = wisdom.encryption(input)
